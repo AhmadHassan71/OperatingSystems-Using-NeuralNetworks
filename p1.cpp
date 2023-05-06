cb028cb3e6f3fb917aed69429babc40eabcf6380
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2023-05-06 22:35:23 +0500

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -8,45 +8,49 @@
 
 #define BUFF_SIZE 100
 
-int pipe1[2],pipe2[2],pipe3[2],pipe4[2];    // pipe1 for input, pipe2 for hidden layer, pipe3 for calculation layer, pipe4 for output layer
-sem_t s1,s2,s3,s4;                          // s1 for input, s2 for hidden layer, s3 for calculation layer, s4 for output layer
-pthread_mutex_t m1,m2;
+int pipe1[2], pipe2[2], pipe3[2], pipe4[2]; // pipe1 for input, pipe2 for hidden layer, pipe3 for calculation layer, pipe4 for output layer
+sem_t s1, s2, s3, s4;                       // s1 for input, s2 for hidden layer, s3 for calculation layer, s4 for output layer
+pthread_mutex_t m1, m2;
 
-void* readInput(void* args){
+void *readInput(void *args)
+{
     float input_arr[2];
     // Read input from user
-    std::cout<<"Enter Weight 1 : ";
-    std::cin>>input_arr[0];
-    std::cout<<"Enter Weight 2 : ";
-    std::cin>>input_arr[1];
+    std::cout << "Enter Weight 1 : ";
+    std::cin >> input_arr[0];
+    std::cout << "Enter Weight 2 : ";
+    std::cin >> input_arr[1];
 
     // Write input to pipe1
-    write(pipe1[1],input_arr,sizeof(input_arr));
+    write(pipe1[1], input_arr, sizeof(input_arr));
     close(pipe1[1]);
     pthread_exit(NULL);
 }
 
-void* forwardLayer(void* args){
+void *forwardLayer(void *args)
+{
     float input_arr[2];
     // Read input from pipe1
-    read(pipe1[0],input_arr,sizeof(input_arr));
+    read(pipe1[0], input_arr, sizeof(input_arr));
     close(pipe1[0]);
 
     // Create matrix
-    // Reading format is [a b c d e f g h] without the brackets and 
+    // Reading format is [a b c d e f g h] without the brackets and
     // values are separated by spaces
-    std::string filename="Hidden_Weights.txt";
+    std::string filename = "Hidden_Weights.txt";
     float Hidden_Weights[8][8];
-    FILE* fp = fopen(filename.c_str(),"r");
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            fscanf(fp,"%f",&Hidden_Weights[i][j]);
+    FILE *fp = fopen(filename.c_str(), "r");
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            fscanf(fp, "%f", &Hidden_Weights[i][j]);
         }
     }
     fclose(fp);
 
     // Write matrix to pipe2
-    write(pipe2[1],Hidden_Weights,sizeof(Hidden_Weights));
+    write(pipe2[1], Hidden_Weights, sizeof(Hidden_Weights));
     close(pipe2[1]);
     pthread_exit(NULL);
 }
@@ -86,34 +90,33 @@ Output layer:
 int main()
 {
 
-    //create semaphores
-    sem_init(&s1,0,0);
-    sem_init(&s2,0,0);
+    // create semaphores
+    sem_init(&s1, 0, 0);
+    sem_init(&s2, 0, 0);
 
-    //create pipes
+    // create pipes
     pipe(pipe1);
     pipe(pipe2);
 
+    pthread_t readThread, forwardThread, t3, t4;
 
-    pthread_t readThread,forwardThread,t3,t4;
-
-    //set thread attributes
-    pthread_attr_t attr,attr2;
+    // set thread attributes
+    pthread_attr_t attr, attr2;
     pthread_attr_init(&attr);
     pthread_attr_init(&attr2);
-    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
-    pthread_attr_setdetachstate(&attr2,PTHREAD_CREATE_DETACHED);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_DETACHED);
 
-    //set thread priority
+    // set thread priority
     const struct sched_param param = {1};
-    pthread_setschedparam(readThread,SCHED_FIFO,&param);
-    pthread_setschedparam(forwardThread,SCHED_FIFO,&param);
+    pthread_setschedparam(readThread, SCHED_FIFO, &param);
+    pthread_setschedparam(forwardThread, SCHED_FIFO, &param);
 
-    //create threads
-    pthread_create(&readThread,NULL,readInput,NULL);
-    pthread_create(&forwardThread,NULL,forwardLayer,NULL);
+    // create threads
+    pthread_create(&readThread, NULL, readInput, NULL);
+    pthread_create(&forwardThread, NULL, forwardLayer, NULL);
 
-    //wait for threads to finish
+    // wait for threads to finish
     sem_wait(&s1);
     sem_wait(&s2);
     return 0;
