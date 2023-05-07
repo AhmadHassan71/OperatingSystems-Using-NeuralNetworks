e156753f3a24019b9f373d98a8e94bc1f350e149
Muhammad Affan <affan4321@gmail.com>
2023-05-07 22:56:14 +0500

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -1,19 +1,81 @@
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <fcntl.h>
 
 #define BUFF_SIZE 100
 
-sem_t s1,s2,s3;
-pthread_mutex_t m1,m2;
+// pipe1 for input, pipe2 for hidden layer, pipe3 for calculation layer, pipe4 for output layer
+std::string _pipe1 = "pipe1";
+std::string _pipe2 = "pipe2"; 
+std::string _pipe3 = "pipe3";
+std::string _pipe4 = "pipe4";
 
+sem_t s1, s2, s3, s4;        // s1 for input, s2 for hidden layer, s3 for calculation layer, s4 for output layer
+pthread_mutex_t m1, m2;
+
+void *readInput(void *args)
+{
+    float input_arr[2];
+    // Read input from user
+    std::cout << "Enter Weight 1 : ";
+    std::cin >> input_arr[0];
+    std::cout << "Enter Weight 2 : ";
+    std::cin >> input_arr[1];
+
+    // Write input to pipe1
+    int fd=open(_pipe1.c_str(),O_WRONLY);
+    write(fd, input_arr, sizeof(input_arr));
+    close(fd);
+    pthread_exit(NULL);
+}
+
+void *forwardLayer(void *args)
+{
+    float input_arr[2];
+    char input_array[BUFF_SIZE];
+    int fd=open(_pipe1.c_str(),O_RDONLY);
+    // Read input from pipe1
+    read(fd, input_arr, sizeof(input_arr));
+    close(fd);
+    std::cout<<"\nInput array : "<<input_arr[0]<<" "<<input_arr[1]<<std::endl;
+
+
+    // Create matrix
+    // Reading format is [a b c d e f g h] without the brackets and
+    // values are separated by spaces
+    std::string filename = "Hidden_Weights.txt";
+    float Hidden_Weights[8][8];
+    FILE *fp = fopen(filename.c_str(), "r");
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            fscanf(fp, "%f", &Hidden_Weights[i][j]);
+        }
+    }
+    fclose(fp);
+
+    // Write matrix to pipe2
+    int fd2;
+    fd2=open(_pipe2.c_str(),O_WRONLY);
+    write(fd2, Hidden_Weights, sizeof(Hidden_Weights));
+    close(fd2);
+    sem_post(&s1);
+    pthread_exit(NULL);
+}
+
+void* hiddenLayer(void* args){
+
+    pthread_exit(NULL);
+}
 /*
 
 Creation of a neural network with 3 layers:
-
 
 Input layer:
 input  -> 2 weights
@@ -28,7 +90,7 @@ Hidden layer:
     create Matrix
     pipe2( use between ForwardLayer and CalcculationLayer)
 
-    CalcculationLayer:
+    CalculationLayer:
             -->Backpropagation:
     {0,1,2,3,4}-> exec -> process -> mat[0]->thread mat[1]->thread...
     (communication between files through pipes)
@@ -43,3 +105,37 @@ Output layer:
         weights -> output
 
 */
+
+int main()
+{
+
+    // create semaphores
+    sem_init(&s1, 0, 0);
+
+    // create pipes
+    mkfifo(_pipe1.c_str(), 0666);
+    mkfifo(_pipe2.c_str(), 0666);
+
+    pthread_t readThread, forwardThread, t3, t4;
+
+    // set thread attributes
+    pthread_attr_t attr, attr2;
+    pthread_attr_init(&attr);
+    pthread_attr_init(&attr2);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_DETACHED);
+
+    // set thread scheduling policy
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    pthread_attr_setschedpolicy(&attr2, SCHED_FIFO);
+
+    // create threads
+    pthread_create(&readThread, NULL, readInput, NULL);
+    pthread_create(&forwardThread, NULL, forwardLayer, NULL);
+
+    // wait for threads to finish
+    sem_wait(&s1);
+    unlink(_pipe1.c_str());
+    unlink(_pipe2.c_str());
+    return 0;
+}
