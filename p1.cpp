48d5caffc455f2d5ebc3f0c7c1cd7ec90ab312fc
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2023-05-07 22:44:02 +0500

    how

--- p1.cpp
+++ p1.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <fcntl.h>
 
 #define BUFF_SIZE 100
 
-sem_t s1,s2,s3;
-pthread_mutex_t m1,m2;
+// pipe1 for input, pipe2 for hidden layer, pipe3 for calculation layer, pipe4 for output layer
+std::string _pipe1 = "pipe1";
+std::string _pipe2 = "pipe2";
+std::string _pipe3 = "pipe3";
+std::string _pipe4 = "pipe4";
 
-<<<<<<< Updated upstream
-=======
 sem_t s1, s2, s3, s4; // s1 for input, s2 for hidden layer, s3 for calculation layer, s4 for output layer
 pthread_mutex_t m1, m2;
 const int neurons = 8;
@@ -35,6 +38,7 @@ void *readInput(void *args)
     int fd = open(_pipe1.c_str(), O_WRONLY);
     write(fd, input_arr, sizeof(input_arr));
     close(fd);
+    pthread_exit(NULL);
 }
 
 void *inputLayer(void *args)
@@ -68,38 +72,68 @@ void *inputLayer(void *args)
     write(fd2, Hidden_Weights, sizeof(Hidden_Weights));
     close(fd2);
     sem_post(&s2);
+    pthread_exit(NULL);
 }
->>>>>>> Stashed changes
 /*
-
 Creation of a neural network with 3 layers:
-
-
 Input layer:
 input  -> 2 weights
 arr[2] -> weight1, weight2
 pipe1  -> arr[2]
 call hidden layer
-
 Hidden layer:
     ForwardLayer
             -->Forward propogation
     read from pipe1
     create Matrix
     pipe2( use between ForwardLayer and CalcculationLayer)
-
-    CalcculationLayer:
+    CalculationLayer:
             -->Backpropagation:
     {0,1,2,3,4}-> exec -> process -> mat[0]->thread mat[1]->thread...
     (communication between files through pipes)
-
     pipe3(use between CalcculationLayer and ReturnResultLayer)
     ReturnResultLayer:
         calculated gradients -> f(x) -> send to output layer
-
     pipe4(use between ReturnResultLayer and OutputLayer)
-
 Output layer:
         weights -> output
-
 */
+
+/*
+    input layer
+*/
+
+int main()
+{
+
+    // create semaphores
+    sem_init(&s1, 0, 0);
+
+    // create pipes
+    mkfifo(_pipe1.c_str(), 0666);
+    mkfifo(_pipe2.c_str(), 0666);
+
+    pthread_t readThread, inputThread, t3, t4;
+
+    // set thread attributes
+    pthread_attr_t attr, attr2;
+    pthread_attr_init(&attr);
+    pthread_attr_init(&attr2);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_DETACHED);
+
+    // set thread scheduling policy
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    pthread_attr_setschedpolicy(&attr2, SCHED_FIFO);
+
+    // create threads
+    pthread_create(&readThread, NULL, readInput, NULL);
+    pthread_create(&inputThread, NULL, inputLayer, NULL);
+
+    // wait for threads to finish
+    sem_wait(&s1);
+    sem_wait(&s2);
+    unlink(_pipe1.c_str());
+    unlink(_pipe2.c_str());
+    return 0;
+}
